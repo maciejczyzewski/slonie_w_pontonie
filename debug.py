4e75fa70237aedf4bda781b9fe62a6dd4bff1b85
Maciej A. Czyzewski <maciejanthonyczyzewski@gmail.com>
2019-04-13 20:16:35 +0200

    add: 3d estymacja postury

--- debug.py
+++ debug.py
@@ -7,32 +7,50 @@ import joints
 import toolbox
 from toolbox import pt, pts
 
-def visualize(img, proc_param, joints, verts, cam):
+
+def visualize(img, proc_param, _joints, verts, cam):
     cam_for_render, vert_shifted, joints_orig = vis_util.get_original(
-        proc_param, verts, cam, joints, img_size=img.shape[:2])
+        proc_param, verts, cam, _joints, img_size=img.shape[:2])
+
+    mask = np.zeros(img.shape, dtype=np.uint8)
+    renderer = vis_util.SMPLRenderer(face_path=joints.config.smpl_face_path)
+    rend_img_overlay = renderer(
+        vert_shifted, cam=cam_for_render, img=mask, do_alpha=False)
+    
+    # rend_img_overlay[0<rend_img_overlay] = 1
+
+    black_pixels_mask = np.all(rend_img_overlay == [0, 0, 0], axis=-1)
+    non_black_pixels_mask = np.any(rend_img_overlay != [0, 0, 0], axis=-1)
+    # or non_black_pixels_mask = ~black_pixels_mask
+    
+    image_copy = rend_img_overlay.copy()
+    image_copy[black_pixels_mask] = [255, 255, 255]
+    image_copy[non_black_pixels_mask] = [0, 0, 0]
+    
+    #kernel = np.ones((2,2),np.uint8)
+    #image_copy = cv2.erode(image_copy,kernel,iterations = 1)
+
+    image_copy = ~image_copy
+    preimg = cv2.bitwise_and(img, image_copy)
+
+    toolbox.debug([rend_img_overlay, image_copy, preimg])
 
     skel_img = vis_util.draw_skeleton(img, joints_orig)
 
-    """
-    x = toolbox.line_crop(skel_img, \
-        pt(joints_orig[9]), pt(joints_orig[10]), size=35)
-    toolbox.debug(x)
-    """
-
     x = toolbox.crop(skel_img,
-            toolbox.pad(
-                toolbox.inject(joints_orig, [2, 3, 8, 9]),
-                size=0
-            ),
-            warped=True)
+                     toolbox.pad(
+                         toolbox.inject(joints_orig, [2, 3, 8, 9]),
+                         size=0
+                     ),
+                     warped=True)
     y = toolbox.crop(skel_img,
-            toolbox.pad(
-                toolbox.inject(joints_orig, [2, 3, 8, 9]),
-                size=-20
-            ),
-            warped=True)
-    toolbox.debug([x, y, skel_img])
-    #toolbox.debug(skel_img)
+                     toolbox.pad(
+                         toolbox.inject(joints_orig, [2, 3, 8, 9]),
+                         size=-20
+                     ),
+                     warped=True)
+    #toolbox.debug([x, y, skel_img])
 
-img, proc_param, joints, verts, cams = joints.parse("data/lewy.jpg")
-visualize(img, proc_param, joints, verts, cams)
+
+img, proc_param, _joints, verts, cams = joints.parse("data/lewy.jpg")
+visualize(img, proc_param, _joints, verts, cams)
