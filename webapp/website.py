536d0fcbdcaa3cbf3488e271243fe5304beadd8e
mareksubocz <marsub@wp.pl>
2019-04-13 23:29:00 +0200

    webapp scripts

--- webapp/website.py
+++ webapp/website.py
@@ -3,22 +3,33 @@ import webbrowser
 f = open('helloworld.html', 'w')
 
 message = """
-<!doctype html>
 <html lang=”en”>
 <head>
-<meta charset="utf-8">
-<meta http-equiv="x-ua-compatible" content="ie=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- Name of your awesome camera app -->
-<title>Camera App</title>
-<!-- Link to your main style sheet-->
-<link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Name of your awesome camera app -->
+    <title>Camera App</title>
+    <!-- Link to your main style sheet-->
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<!-- Reference to your JavaScript file -->
-<script src="app.js"></script>
+    <!-- Camera -->
+    <main id="camera">
+        <!-- Camera sensor -->
+        <canvas id="camera--sensor"></canvas>
+        <!-- Camera view -->
+        <video id="camera--view" autoplay playsinline></video>
+        <!-- Camera output -->
+        <img src="//:0" alt="" id="camera--output">
+        <!-- Camera trigger -->
+        <button id="camera--trigger">Take a picture</button>
+    </main>
+    <!-- Reference to your JavaScript file -->
+    <script src="app.js"></script>
 </body>
 </html>
+
 """
 
 f.write(message)
