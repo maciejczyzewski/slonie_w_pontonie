9ef504b223b8427c7ade94155c8c059ffb941abb
Maciej A. Czyzewski <maciejanthonyczyzewski@gmail.com>
2019-04-14 05:51:49 +0200

    fix: generacja serii danych (input.py) -> results

--- input.py
+++ input.py
@@ -1,3 +1,5 @@
+"""generuje dane "results" dla serii danych z input_series/"""
+
 # czyta z input_series/
 import keras
 from keras.datasets import mnist
@@ -14,6 +16,8 @@ from tqdm import tqdm
 from glob import glob
 
 # FIXME: jakas obsluga PudzianNet
+
+DEBUG = False
 
 from PudzianNet import net_names, model, f1_score
 
@@ -38,14 +42,15 @@ def __action_hash(obj):
 
 print("[[[INIT]]]")
 from copy import copy
-models = {}
-for net_name in net_names:
-    print("wczytuje {}".format(net_name))
-    #models[net_name] = load_model("model/"+net_name, custom_objects={'f1_score':
-    #    f1_score})
-    models[net_name] = copy(model)
-    models[net_name].load_weights("model/"+net_name)
-print("------------")
+if DEBUG:
+    models = {}
+    for net_name in net_names:
+        print("wczytuje {}".format(net_name))
+        #models[net_name] = load_model("model/"+net_name, custom_objects={'f1_score':
+        #    f1_score})
+        models[net_name] = copy(model)
+        models[net_name].load_weights("model/"+net_name)
+    print("------------")
 
 import cv2
 from skimage.transform import resize
@@ -62,26 +67,35 @@ def dynamic_size(img):
     img = cv2.resize(img, (box[1], box[0]))
     #img = resize(img, box, anti_aliasing=True)*255 # FIXME:? *255
     #img = img.astype('int')
-    print(img)
     #toolbox.debug(img)
     return img
 
-from muscles import parse_file_for_muscles
+results = []
 
+from muscles import parse_file_for_muscles
+import os
 from skimage import color
-for filename in tqdm(glob("input_series/*")): #FIXME: tylko pierwsze narazie
+for filename in tqdm(sorted(glob("input_series/*"))): #FIXME: tylko pierwsze narazie
     print(filename)
     img = io.imread(filename)
     #print(img)
     img = dynamic_size(img)
     imgs_muscles = parse_file_for_muscles(img)
 
+    cur_results = {}
+
     for net_name in imgs_muscles:
         img = imgs_muscles[net_name]
         img = color.rgb2gray(img)
 
-        dhash = __action_hash(img)
-
+        valhash = __action_hash(img)
+        valpath = "cache2/" + str(valhash)
+        print("valhash={}".format(valhash))
+        if os.path.isfile(valpath + ".npz"):
+            result = np.load(valpath + ".npz")["result"]
+            print(result)
+            cur_results[net_name] = result
+            continue
         # FIXME: ------------------------------
 
         x_input = np.array([img])
@@ -95,5 +109,11 @@ for filename in tqdm(glob("input_series/*")): #FIXME: tylko pierwsze narazie
         result = models[net_name].predict(x_input)[0]
         result = [round(result[0], 3), round(result[1], 3), round(result[2], 3)]
         print("[{}] net_name={}".format(filename, net_name), result)
+
+        np.savez_compressed(valpath, result=result)
+        cur_results[net_name] = result
+
+    results.append(cur_results)
     print("========")
 
+np.savez_compressed("results", results=results)
